b522dd57639ad4ec74f5a1dd4ca426d401fe65f8
마로빔 <be1wer0815@gmail.com>
2025-05-27 09:53:29 +0900

    TrajectoryVisualizer.kt 업데이트
    투사체 궤적 안보이는 버그 수정

--- src/main/kotlin/com/io/github/marobim815/projectileQuadraticFunctionPlugin/TrajectoryVisualizer.kt
+++ src/main/kotlin/com/io/github/marobim815/projectileQuadraticFunctionPlugin/TrajectoryVisualizer.kt
@@ -1,17 +1,20 @@
 package com.io.github.marobim815.projectileQuadraticFunctionPlugin
 
 import org.bukkit.Bukkit
-import org.bukkit.Color
+import org.bukkit.Location
 import org.bukkit.Particle
 import org.bukkit.plugin.java.JavaPlugin
 
 class TrajectoryVisualizer(private val plugin: JavaPlugin) {
 
-    fun draw(startLoc: org.bukkit.Location, trajectory: Trajectory) {
+    fun draw(startLoc: Location, trajectory: Trajectory) {
         Bukkit.getScheduler().runTask(plugin, Runnable {
             for (point in trajectory.points) {
                 val loc = startLoc.clone().add(point.x, point.y, point.z)
-                startLoc.world?.spawnParticle(Particle.HEART, loc, 1, Particle.DustOptions(Color.RED, 1.0f))
+                startLoc.world?.spawnParticle(Particle.HEART, loc, 1)
+                // 만약 색 입자 원하면 아래처럼 DUST 쓰셈!
+                // val dust = Particle.DustOptions(Color.RED, 1.0f)
+                // startLoc.world?.spawnParticle(Particle.REDSTONE, loc, 0, 0.0, 0.0, 0.0, 1.0, dust)
             }
         })
     }
