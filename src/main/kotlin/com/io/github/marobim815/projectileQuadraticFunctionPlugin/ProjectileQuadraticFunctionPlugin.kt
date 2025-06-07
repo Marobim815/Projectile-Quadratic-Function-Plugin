338e0d3974b74f946cf568a85af888ee11ddbdd5
PC <be1wer0815@gmail.com>
2025-06-07 20:52:40 +0900

    visual fix

--- src/main/kotlin/com/io/github/marobim815/projectileQuadraticFunctionPlugin/ProjectileQuadraticFunctionPlugin.kt
+++ src/main/kotlin/com/io/github/marobim815/projectileQuadraticFunctionPlugin/ProjectileQuadraticFunctionPlugin.kt
@@ -4,6 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class ProjectileQuadraticFunctionPlugin : JavaPlugin() {
     // java -Xmx4G -Xms2G -jar paper-1.21.4-231.jar nogui
+    // cd C:\ChristmasServer
 
     override fun onEnable() {
         logger.info("Hello world!")
